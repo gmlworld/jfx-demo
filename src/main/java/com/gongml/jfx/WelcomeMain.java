6f6218306ce15dfd0db50b17711ff69a7e03ca69
龚茂林 <gongmaolin@fybdp.com>
2020-05-21 09:38:00 +0800

    import bootstrapfx

--- src/main/java/com/gongml/jfx/WelcomeMain.java
+++ src/main/java/com/gongml/jfx/WelcomeMain.java
@@ -2,9 +2,13 @@ package com.gongml.jfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
 
 /**
  * @program: HelloGui
@@ -13,14 +17,24 @@ import javafx.stage.Stage;
 public class WelcomeMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
+//        Panel panel = new Panel("This is the title");
+//        panel.getStyleClass().add("panel-primary");                            //(2)
+//        BorderPane content = new BorderPane();
+//        content.setPadding(new Insets(120));
+//
+//        Button button = new Button("Hello BootstrapFX");
+//        button.getStyleClass().setAll("btn","btn-danger");//(2)
+//
+//        content.setCenter(button);
+//        panel.setBody(content);
+//
+//        Scene scene = new Scene(panel);
+//        scene.getStylesheets().add("org/kordamp/bootstrapfx/bootstrapfx.css");
+
         Parent root = FXMLLoader.load(getClass().getResource("../../../../resources/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
