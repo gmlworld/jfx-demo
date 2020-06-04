93b05f2deb226a82003ed84d651c479a3d911975
gongml <1019753410@qq.com>
2020-06-04 23:53:48 +0800

    添加打包插件

--- src/main/java/com/gongml/jfx/WelcomeMain.java
+++ src/main/java/com/gongml/jfx/WelcomeMain.java
@@ -12,9 +12,11 @@ import javafx.stage.Stage;
 public class WelcomeMain {
 
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../../../../resources/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Scene scene = new Scene(root, 900, 600);
+        scene.getStylesheets().add("org/kordamp/bootstrapfx/bootstrapfx.css");
         primaryStage.setTitle("点餐系统");
-        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
